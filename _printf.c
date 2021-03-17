b4b14a29cde5fd8214d02705636e3c8c3158451f
vasketz <anfevach@gmail.com>
2021-03-17 05:02:07 +0000

    Last commit checked

--- _printf.c
+++ _printf.c
@@ -39,47 +39,40 @@ int (*get_format_function(const char *identifier))(va_list)
  * ... - all agurments passed trough variadic function
  * Return: number of caracters.
  */
-
 int _printf(const char *format, ...)
 {
-    int retractor = 0;
-    int (*pt_func)(va_list);
-    int state = 0;
-    va_list argument_list;
-    va_start(argument_list, format);
-    while (format && *format)
-    {
-        if (*format == '%')
-        {
-            pt_func = get_format_function(++format);
-            if (pt_func)
-            {
-                retractor += pt_func(argument_list);
-            }
-            else
-            {
-                write(1, format - 1, 1);
-                retractor++;
-                write(1, format, 1);
-                retractor++;
-            }
-            if (*format != '%')
-            {
-                format++;
-                state = 0;
-            }
-        }
-        if (*format != '%')
-        {
-            write(1, format, 1);
-            retractor++;
-        }
-		/**state == 0*/
-        if (!state)
-          state = 1;
-        else
-            format++;
-    }
-    va_end(argument_list);
-    return (retractor);
+	int retractor = 0;
+	int (*pt_func)(va_list);
+	va_list argument_list;
+	va_start(argument_list, format);
+	while (format && *format)
+	{
+		if (*format == '%')
+		{
+			pt_func = get_format_function(++format);
+			if (pt_func)
+			{
+				retractor += pt_func(argument_list);
+			}
+			else
+			{
+				write(1, format - 1, 1);
+				retractor++;
+				write(1, format, 1);
+				retractor++;
+			}
+			if (*format != '%')
+			{
+				format++;
+			}
+		}
+		if (*format != '%')
+		{
+			write(1, format, 1);
+			retractor++;
+		}
+		format++;
+	}
+	va_end(argument_list);
+	return (retractor);
 }
