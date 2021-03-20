ebadacc18918d2170d18daa98b7ee291e2aa6b2a
vasketz <anfevach@gmail.com>
2021-03-20 04:26:31 +0000

    Update header

--- holberton.h
+++ holberton.h
@@ -45,6 +45,5 @@ int print_porcent(va_list argument_list);
 int (*get_format_function(const char *identifier))(va_list);
 
 int print_integer(va_list integer);
-
 unsigned int get_absolut(int num);
 #endif /**HOLBERTON_H*/
